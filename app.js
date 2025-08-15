e19e579d6542e2ed7c3b2194589d9182493a5add
UmeshMehta1 <ukmehta1245@gmail.com>
2025-08-15 21:09:29 +0545

    nodemon

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express")
 
+// import express from "express"
 const app = express()
 
-app.get('/helloworld', (req, res)=>{
-   res.send("<h1>hello word </h1>");
+
+
+app.get("/contact", function(req, res){
+    res.json({
+        "name":"umesh",
+        "age":23,
+        "contact number":9989879879,
+        "email":"hello@gail.com"
+    })
 })
 
+
+app.get("/contact1",(req, res)=>{
+    res.json({
+        "name":"umesh",
+        "age":23,
+        "contact number":9989879879,
+        "email":"hello@gail.com"
+    })
+})
 
 
 app.listen(4000,(req, res)=>{
